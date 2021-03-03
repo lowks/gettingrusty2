2ebe0b040a233c9ad708e669cd54536fede08feb
Muhammad Lukman Low <lowkster.etiqa@gmail.com>
2021-03-03 23:19:02 +0800

    adding example for nth prime. wip

---
+++ hello_world/src/nprimes.rs
@@ -0,0 +1,13 @@
+pub fn nth(n: u32) -> u32 {
+    let mut count = 0;
+    match prime_true(n) {
+        
+    }
+    unimplemented!("What is the 0-indexed {}th prime number?", n)
+}
+
+fn prime_true(n: u32) -> bool {
+    match n {
+        
+    }
+}
