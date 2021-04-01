0e194016da885ee4f30c8097ff5af70ff80e628f
Low Kian Seong <kianseong@gmail.com>
2021-04-01 22:06:40 +0800

    adding panic exmple

---
+++ hello_world/src/panic_example.rs
@@ -0,0 +1,13 @@
+pub fn drink(beverage: &str) {
+    // You shouldn't drink too much sugary beverages.
+    if beverage == "lemonade" { panic!("AAAaaaaa!!!!"); }
+
+    println!("Some refreshing {} is all I need.", beverage);
+}
+
+#[test]
+#[should_panic]
+
+fn test_should_panic() {
+    drink("lemonade");
+}
