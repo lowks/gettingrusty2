4cdee27c56d2f32672ffa3282e859b7c898369e5
Muhammad Lukman Low <lowkster.etiqa@gmail.com>
2021-04-06 23:56:35 +0800

    some_or_none2

--- hello_world/src/some_or_none.rs
+++ hello_world/src/some_or_none.rs
@@ -9,17 +9,32 @@
 /// let d = unwrap_some_or_none(Some("hehe"));
 /// assert_eq!(d, "hehe");
 /// ```
+/// ```
+/// use hello_world::unwrap_some_or_else;
+/// let e = unwrap_some_or_else(None);
+/// assert_eq!(e, "The Dog");
+/// ```
 
 pub fn unwrap_some_or_none(input: Option<&'static str>) -> String {
     input.unwrap_or("Toto").to_string()
+}
+
+pub fn unwrap_some_or_else(input: Option<&'static str>) -> &str {
+    input.unwrap_or_else(|| { "The Dog" })
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
     #[test]
-    fn test_match_country_code() {
+    fn test_some_or_none() {
         assert_eq!(unwrap_some_or_none(None), "Toto");
         assert_eq!(unwrap_some_or_none(Some("Hoho")), "Hoho");
+    }
+
+    #[test]
+
+    fn test_some_or_else() {
+        assert_eq!(unwrap_some_or_else(None), "The Dog");
     }
 }
