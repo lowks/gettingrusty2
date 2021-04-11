bc0faeb824f7680472e9aef8951ae6affb3eb338
Muhammad Lukman Low <lowkster.etiqa@gmail.com>
2021-04-11 23:17:35 +0800

    Adding more test and assert using is_err and is_ok

--- hello_world/src/ok_or.rs
+++ hello_world/src/ok_or.rs
@@ -2,7 +2,11 @@
 /// ```
 /// use hello_world::okor;
 /// let c = okor(None);
+/// let d = okor(Some("Dog"));
 /// assert_eq!(c, Err(0));
+/// assert_eq!(d, Ok("Dog"));
+/// assert!(c.is_err());
+/// assert!(d.is_ok());
 /// ```
 
 pub fn okor(input: Option<&'static str>) -> Result<&str, i32> {
@@ -13,5 +17,10 @@ pub fn okor(input: Option<&'static str>) -> Result<&str, i32> {
 
  fn test_some_or_else() {
      assert_eq!(okor(None), Err(0));
+     assert_eq!(okor(Some("Dog")), Ok("Dog"));
+     let bad_result = okor(None);
+     let good_result = okor(Some("Dog"));
+     assert!(bad_result.is_err());
+     assert!(good_result.is_ok());
 }
 
