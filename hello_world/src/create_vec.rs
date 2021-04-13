4cf0618502607d80d51f79fcbb2c643b6a38e7d7
Muhammad Lukman Low <lowkster.etiqa@gmail.com>
2021-04-13 23:14:40 +0800

    example for ok function

--- hello_world/src/create_vec.rs
+++ hello_world/src/create_vec.rs
@@ -1,3 +1,10 @@
+/// # Examples
+/// ```
+/// use hello_world::add_number_to_element;
+/// let d = add_number_to_element(Ok(3));
+/// assert_eq!(d, Ok(20));
+/// ```
+
 pub enum Multiple {
     Integer(i32),
     Blob(String),
@@ -19,10 +26,22 @@ impl std::fmt::Display for Multiple {
 //     let integer = Multiple::Integer(v);
 // }
 
+#[test]
+
+pub fn test_add_number_to_element() {
+    assert_eq!(add_number_to_element(Ok(3)), Ok(20))
+}
+
 pub fn create_vector() -> Vec<Multiple> {
     let v2 = vec![
         Multiple::Integer(100),
         Multiple::Blob("Hello World".to_string()),
     ];
     v2
+}
+
+pub fn add_number_to_element(thing: Result<i32, i32>) -> Result<i32, i32> {
+    thing
+    .and_then(|number| Ok(number + 1))
+    .and_then(|number2| Ok(number2 * 5))
 }
