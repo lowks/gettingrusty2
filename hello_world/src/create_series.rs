3f99c1d28665d311088ba4334d3dfc140aee219d
Muhammad Lukman Low <lowkster.etiqa@gmail.com>
2021-04-29 23:47:34 +0800

    double sum

--- hello_world/src/create_series.rs
+++ hello_world/src/create_series.rs
@@ -11,6 +11,14 @@ fn test_sum_items_in_vec() {
     assert_eq!(sum_items_in_vec(true, vec![1, 2, 3]), 12)
 }
 
+#[test]
+
+fn test_double_and_sum() {
+    assert_eq!(double_and_sum(vec![1,2,3]), 12);
+    assert_eq!(double_and_sum(vec![10, 20]), 60);
+    // assert_eq!(sum_items_in_vec(true, vec![1, 2, 3]), 12)
+}
+
 pub fn create_series(x: i32) -> Vec<i32> {
     let result = vec![x, x + 1, x + 2];
 
@@ -22,4 +30,11 @@ pub fn sum_items_in_vec(double: bool, input_vector: Vec<i32>) -> i32 {
         false  => input_vector.iter().fold(0, |total, next| total + next),
         true => input_vector.iter().map(|x| x * 2).fold(0, |total, next| total + next),
     }
+}
+
+pub fn double_and_sum(mut input_vector: Vec<i32>) -> i32 {
+    // input_vector.iter_mut().for_each(|i| *i *= 2);
+    // input_vector.iter_mut().for_each(|i| *i *= 2).iter().sum();
+    // input_vector.iter().sum()
+    input_vector.iter().fold(0, |total, next| total + 2*next)
 }
