c8f2330de047e6638c8b8e1108db5f46e0abf906
Muhammad Lukman Low <lowkster.etiqa@gmail.com>
2021-05-31 23:03:17 +0800

    planet ptr

--- hello_world/src/pointers.rs
+++ hello_world/src/pointers.rs
@@ -7,10 +7,27 @@ pub fn test_compare_pointer_and_deref() {
     assert_eq!(compare_pointer_and_deref(a, b), true);
 }
 
+#[test]
+
+pub fn test_const_pointer() {
+    assert_eq!(const_pointer(), "earth");
+}
+
 pub fn compare_pointer_and_deref(integer_1: i32, integer_2: &i32) -> bool {
     // if integer_1 == *integer_2 {
     //     return true
     // };
     integer_1 == *integer_2
     // return false
+}
+
+pub fn const_pointer() -> &'static str {
+    let planet: &str = "earth";
+    let planet_ptr: *const &str = &planet as *const &str;
+    unsafe {
+        match *planet_ptr {
+            "earth" => &"earth",
+            _ => &"not earth",
+        }
+    }   
 }
